51dc1b5562861b1c9ed4fae8fa1f379f5b7e3a8a
xiaobo-zhao <94277059+xiaobo-zhao@users.noreply.github.com>
2022-03-04 10:54:43 +0800

    Update Test.java

--- src/Exercise/Test.java
+++ src/Exercise/Test.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-
+         AcWing acWing=new AcWing();
+        int[]net=new int[3];
+        Scanner in=new Scanner(System.in);
+        for (int i=0;i< net.length;i++){
+            net[i]=in.nextInt();
+        }
+        for (int c:acWing.twoSum(net,6)) {
+            System.out.print(c+" ");
+        }
         }
 
 
